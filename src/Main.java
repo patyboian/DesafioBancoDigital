2c62201bd6952686bb2bfcd7fe272a78d2487d37
Patricia Boian <patyboian86@gmail.com>
2025-04-14 14:37:11 -0300

    expert talk 14/04/2025

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)  {
+       Cliente patricia = new Cliente();
+       patricia.setNome("Patricia");
+    	Conta cc = new ContaCorrente(patricia); // ao chamar esse construtor já irá gerar agencia e número
+       cc.depositar(100);
+       
+        
+        Conta poupanca = new ContaPoupanca(patricia); 
+        
+        cc.transferir(100,poupanca);
+    
+       cc.imprimirExtrato();
+       poupanca.imprimirExtrato();
+    
+    }
+}
